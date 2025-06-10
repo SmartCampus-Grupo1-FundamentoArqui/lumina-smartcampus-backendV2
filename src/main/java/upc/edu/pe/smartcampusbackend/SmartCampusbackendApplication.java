b09c183fe1c49807bb158007a28306e5fc573321
NelsonUPC <164695009+NelsonUPC@users.noreply.github.com>
2025-06-09 19:43:06 -0500

    commit: innitial project

---
+++ src/main/java/upc/edu/pe/smartcampusbackend/SmartCampusbackendApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.smartcampusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/edu/pe/smartcampusbackend/SmartCampusbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.smartcampusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
